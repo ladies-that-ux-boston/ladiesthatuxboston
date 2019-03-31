52491c89dde9df2aed00890771ce018563e7e245
ladiesthatuxboston <boston@ladiesthatux.com>
2019-03-31 17:57:21 -0400

    mailing list styling

--- src/components/social.js
+++ src/components/social.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,14 +15,7 @@ const Social = () => (
   <section className={styles.social}>
     <h2 className={styles.textCenter}>Join the conversation</h2>
     <p className={styles.textCenter}>
-      Signup for our
-      <a
-        href="https://us12.list-manage.com/subscribe?u=b96f7627f43794d6c133035bd&id=8c099cce18"
-        target="_blank"
-        rel="noopener norefferer"
-      >
-        mailing list
-      </a>
+      Signup for our <a href="https://us12.list-manage.com/subscribe?u=b96f7627f43794d6c133035bd&id=8c099cce18" target="_blank" rel="noopener norefferer">mailing list</a>
     </p>
     <ul className={styles.iconWrap}>
       <li>
