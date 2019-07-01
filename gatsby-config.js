533b52ee85d4f3d784815713537d7f09217fa66a
langermank <langermank@gmail.com>
2019-06-30 20:37:21 -0400

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-less',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-typography',
       options: {
