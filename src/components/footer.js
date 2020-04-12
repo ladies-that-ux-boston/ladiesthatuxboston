4d23bf418ab1e27e8461ba2582c47c7b6b11de55
langermank <langermank@gmail.com>
2020-04-12 12:19:26 -0400

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,61 +1,31 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import styles from '../styles/footer.module.less';
+import PropTypes from "prop-types";
+import React from "react";
+import styles from "../styles/footer.module.less";
+import Logo from "./logo";
 
 const Footer = () => (
-	<footer>
-		<p>Katie Langerman</p>
-		<ul className={styles.navLinksWrap}>
-			<li className={styles.navLinks}>
-				<a href="mailto:langermank@gmail.com" alt="email katie">
-					langermank@gmail.com
-				</a>
-			</li>
-			<li className={styles.navLinks}>
-				<a
-					href="https://www.linkedin.com/in/langermank/"
-					alt="linkedin"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					linkedin
-				</a>
-			</li>
-			<li className={styles.navLinks}>
-				<a
-					href="https://twitter.com/KatieLangerman"
-					alt="twitter"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					twitter
-				</a>
-			</li>
-			<li className={styles.navLinks}>
-				<a href="https://github.com/langermank" alt="github" target="_blank" rel="noopener noreferrer">
-					github
-				</a>
-			</li>
-			<li className={styles.navLinks}>
-				<a
-					href="https://dribbble.com/katielangerman"
-					target="_blank"
-					rel="noopener noreferrer"
-					alt="dribbble"
-				>
-					dribbble
-				</a>
-			</li>
-		</ul>
-	</footer>
+  <footer>
+    <Logo />
+    <div className={styles.footerFlex}>
+      <p>Ladies that UX Boston</p>
+      <a
+        href="https://docs.google.com/document/d/1w05mQwGxsBzrg1zGs_iV8oy_B5o15efXsaAOtVgngWg/edit?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="code of conduct"
+      >
+        code of conduct
+      </a>
+    </div>
+  </footer>
 );
 
 Footer.propTypes = {
-	siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string
 };
 
 Footer.defaultProps = {
-	siteTitle: '',
+  siteTitle: ""
 };
 
 export default Footer;
