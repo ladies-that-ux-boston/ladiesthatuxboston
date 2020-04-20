ec8443d6a048e0d87ae0c1ffb1dc4a7930ec1d2b
langermank <langermank@gmail.com>
2020-04-19 22:48:43 -0400

    section style starts

--- src/components/mediumfeed.js
+++ src/components/mediumfeed.js
@@ -2,25 +2,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
+import styles from "../styles/mediumposts.module.less";
 
-const mediumCDNUrl = `https://cdn-images-1.medium.com/max/150/`;
+const mediumCDNUrl = `https://cdn-images-1.medium.com/max/600/`;
 
 export default function MediumFeed({ data }) {
   const posts = data.allMediumPost.edges;
 
   return (
-    <section>
-      {posts.map(post => (
-        <article key={post.node.id}>
-          <h2>{post.node.title}</h2>
-          <h3>by {post.node.author.name}</h3>
-          <img
-            src={`${mediumCDNUrl}/${post.node.virtuals.previewImage.imageId}`}
-            alt={post.node.title}
-            width="150"
-          />
-        </article>
-      ))}
+    <section className={styles.fullWidth}>
+      <div className={styles.postsWrap}>
+        {posts.map(post => (
+          <article key={post.node.id}>
+            {/* <h3>by {post.node.author.name}</h3> */}
+            {/* <div className={styles.postImgWrap}> */}
+            <img
+              src={`${mediumCDNUrl}/${post.node.virtuals.previewImage.imageId}`}
+              alt={post.node.title}
+              className={styles.postImg}
+            />
+            <p>{post.node.title}</p>
+            {/* </div> */}
+          </article>
+        ))}
+      </div>
     </section>
   );
 }
